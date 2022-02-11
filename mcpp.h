2e2205f8eef4a37742dc97cc62979e4d0e79ee4a
PunkyMunky64 <46407361+PunkyMunky64@users.noreply.github.com>
2022-02-10 20:41:50 -0800

    V1.0.0

---
+++ mcpp.h
@@ -0,0 +1,69 @@
+#pragma once
+//WELCOME TO MC++! Where everything is the same as c++ but it's easier to read. All the documentation you need is right here. Remember to not use these words for other functions and also don't use "using namespace" statements that could conflict with these.
+
+#include <iostream>
+
+#define plus +
+#define minus -
+#define multiplied *
+#define divided /
+#define bitleft <<
+#define bitright >>
+
+#define increment ++
+#define decrement --
+#define adds +=
+#define subtracts -=
+#define multiplies *=
+#define divides /=
+
+#define at *
+#define address &
+#define his .
+#define scope ::
+#define knows ->
+
+#define assigns =
+#define assign =
+#define gets =
+#define set =
+#define value auto
+
+//english connectors that do nothing
+#define by 
+#define than
+#define to
+#define is
+
+#define also ,
+#define over ;
+#define done ;
+
+#define output std::cout <<
+#define input std::cin >>
+#define newline << '\n';
+#define lib std::
+
+#define quantity (
+#define endquantity )
+#define statements {
+#define endstatements }
+#define index [
+#define endindex ]
+#define form <
+#define endform >
+
+#define less <
+#define greater >
+#define equal ==
+#define atmost <=
+#define atleast >=
+
+#define condition if (
+#define aslongas while (
+#define iterate for(
+#define then )
+#define otherwise } else {
+
+#define start int main() {
+#define end return 0;}
